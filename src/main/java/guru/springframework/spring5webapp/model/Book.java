64e774921e5edf226e104fc82901f64efe523e00
Roman Arkhipov <aar.rumm@gmail.com>
2020-04-27 22:47:16 +0200

    * remove infinite loop in toString

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -100,7 +100,6 @@ public class Book {
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
                 ", publisher='" + publisher + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 }
